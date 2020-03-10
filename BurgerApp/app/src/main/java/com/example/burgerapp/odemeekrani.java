ab90f3440017fa49c85839f127a6e7c6506fa920
aliaydin96 <h.aliaydin2@gmail.com>
2020-03-10 22:59:23 +0300

    Toast mesaji eklendi.

--- BurgerApp/app/src/main/java/com/example/burgerapp/odemeekrani.java
+++ BurgerApp/app/src/main/java/com/example/burgerapp/odemeekrani.java
@@ -3,14 +3,23 @@ package com.example.burgerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 public class odemeekrani extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_odemeekrani);
-
+        Button odemeyap = findViewById(R.id.odemeyap);
+        odemeyap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Odeme islemi tamamlandi.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
