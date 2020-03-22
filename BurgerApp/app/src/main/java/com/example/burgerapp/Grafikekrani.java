13c358b059feb592985d238af9d2fb489d2627ea
aliaydin96 <h.aliaydin2@gmail.com>
2020-03-22 21:50:13 +0300

    Grafikler cizdirildi

--- BurgerApp/app/src/main/java/com/example/burgerapp/Grafikekrani.java
+++ BurgerApp/app/src/main/java/com/example/burgerapp/Grafikekrani.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -21,6 +22,8 @@ import java.util.List;
 public class Grafikekrani extends AppCompatActivity {
     BarChart barChart;
     FoodDatabaseHelper foodDatabaseHelper;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,14 +31,55 @@ public class Grafikekrani extends AppCompatActivity {
         foodDatabaseHelper = new FoodDatabaseHelper(this);
         final List<String> foodData = foodDatabaseHelper.getFoodData();
         int counter = 0;
-        int bbqburger_number = 0;
-        int mushroomburger_number = 0;
+        int bbqburger_counter = 0;
+        int mushroomburger_counter = 0;
+        int doublecheeseburger_counter = 0;
+        int cheeseburger_counter = 0;
+        int chickenburger_counter = 0;
+        int classicburger_counter = 0;
+        int su_counter = 0;
+        int ayran_counter = 0;
+        int fanta_counter = 0;
+        int kola_counter = 0;
+        int sprite_counter = 0;
+        int icetea_counter = 0;
+
         while(foodData.size() > counter){
-            if(foodData.get(counter) == "BBQ Burger"){
-                bbqburger_number += Integer.parseInt(foodData.get(counter+1));
+            if(foodData.get(counter).contentEquals("BBQ Burger")){
+                bbqburger_counter += Integer.parseInt(foodData.get(counter+1));
             }
-            if(foodData.get(counter) == "Mushroom Burger"){
-                mushroomburger_number += Integer.parseInt(foodData.get(counter+1));
+            if(foodData.get(counter).contentEquals("Mushroom Burger")){
+                mushroomburger_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Double Cheese Burger")){
+                doublecheeseburger_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Cheese Burger")){
+                cheeseburger_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Chicken Burger")){
+                chickenburger_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Classic Burger")){
+                classicburger_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Su")){
+                su_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Ayran")){
+                ayran_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Fanta")){
+                fanta_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Kola")){
+                kola_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Sprite")){
+                sprite_counter += Integer.parseInt(foodData.get(counter+1));
+            }
+            if(foodData.get(counter).contentEquals("Icetea")){
+                icetea_counter += Integer.parseInt(foodData.get(counter+1));
             }
             counter++;
         }
@@ -47,23 +91,47 @@ public class Grafikekrani extends AppCompatActivity {
         barChart.setPinchZoom(false);
         barChart.setDrawGridBackground(true);
 
+
+
         ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(1,40f));
-        barEntries.add(new BarEntry(2,44f));
-        barEntries.add(new BarEntry(3,30f));
-        barEntries.add(new BarEntry(4,36f));
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Dates");
+        barEntries.add(new BarEntry(1, bbqburger_counter));
+        barEntries.add(new BarEntry(2, mushroomburger_counter));
+        barEntries.add(new BarEntry(3, doublecheeseburger_counter));
+        barEntries.add(new BarEntry(4, cheeseburger_counter));
+        barEntries.add(new BarEntry(5, chickenburger_counter));
+        barEntries.add(new BarEntry(6, classicburger_counter));
+        barEntries.add(new BarEntry(7, su_counter));
+        barEntries.add(new BarEntry(8, ayran_counter));
+        barEntries.add(new BarEntry(9, fanta_counter));
+        barEntries.add(new BarEntry(10, kola_counter));
+        barEntries.add(new BarEntry(11, sprite_counter));
+        barEntries.add(new BarEntry(12, icetea_counter));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Food Name");
         barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+//        List<String> labels = new ArrayList<String>();
+       // labels.add("BBQ Burger");
+       // labels.add("Mushroom Burger");
 
 
         BarData data = new BarData(barDataSet);
-        data.setBarWidth(1);
+        data.setBarWidth(0.5f);
         barChart.setData(data);
+        barChart.setFitBars(true);
 
-        String[] months = new String[]{"Jan", "Feb", "Mar", "April"};
+
         XAxis xAxis = barChart.getXAxis();
-        xAxis.setValueFormatter(new MyXAxisValueFormatter(months));
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setAxisMinimum(0);
+
+
+        YAxis yAxis = barChart.getAxisLeft();
+        yAxis.setAxisMinimum(0);
+        barChart.getAxisRight().setEnabled(false);
+
+
+
 /*
         barChart.setTouchEnabled(true);
         barChart.setDragEnabled(true);
@@ -72,17 +140,6 @@ public class Grafikekrani extends AppCompatActivity {
 
     }
 
-    public class MyXAxisValueFormatter extends ValueFormatter implements IAxisValueFormatter{
-        private String[] mValues;
-        public MyXAxisValueFormatter(String[] values){
-            this.mValues = values;
-        }
-
-        @Override
-        public String getFormattedValue(float value, AxisBase axis) {
-            return mValues[(int)value];
-        }
-    }
 
 
 }
